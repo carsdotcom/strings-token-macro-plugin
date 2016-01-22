460169c2edd3bf9c37799d5d085f98f48edd5560
Keith Kreissl <kkreissl@cars.com>
2016-01-22 07:41:25 -0600

    PLAT-9999 | First attempt at creating a plugin for jenkins

---
+++ src/main/java/com/cars/plugin/token/strings/SubstringTokenMacro.java
@@ -0,0 +1,48 @@
+package com.cars.plugin.token.strings;
+
+import java.io.IOException;
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+import org.jenkinsci.plugins.tokenmacro.DataBoundTokenMacro;
+import org.jenkinsci.plugins.tokenmacro.MacroEvaluationException;
+
+import hudson.Extension;
+import hudson.model.AbstractBuild;
+import hudson.model.TaskListener;
+
+@Extension
+public class SubstringTokenMacro extends DataBoundTokenMacro {
+	
+	@Parameter(required = true)
+	public String value = "";
+	
+	@Parameter(required = false)
+	public int start = 0;
+	
+	@Parameter(required = true)
+	public int end = 0;
+	
+
+	@Override
+	public String evaluate(AbstractBuild<?, ?> context, TaskListener listener, String macroName)
+			throws MacroEvaluationException, IOException, InterruptedException {
+		String v = value;
+		System.out.println("I MADE IT IN HERE");
+		if (StringUtils.contains(value, "env:")){
+			String key = StringUtils.substringAfter(value, "env:");
+			Map<String, String> env = context.getBuildVariables();
+			v = env.get(key);
+		}
+		
+		return StringUtils.substring(v, start, end);
+	}
+
+	@Override
+	public boolean acceptsMacroName(String name) {
+		System.out.println("I AM BEING CALLED");
+		// TODO Auto-generated method stub
+		return "SUBSTRING".equals(name);
+	}
+
+}
